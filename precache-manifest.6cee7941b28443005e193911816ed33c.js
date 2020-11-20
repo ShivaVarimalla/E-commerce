9198f9de0e8caca6199c9adc2a990a0b06faa2ab
shivashankar1253 <varimallashankar@gmail.com>
2020-11-20 13:15:19 +0530

    Updates

---
+++ precache-manifest.6cee7941b28443005e193911816ed33c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4486fd003face8d7a9794227dbedb48",
+    "url": "/e-commere/index.html"
+  },
+  {
+    "revision": "6301f193b5cc17df5dbd",
+    "url": "/e-commere/static/css/main.5bff10ad.chunk.css"
+  },
+  {
+    "revision": "ffe3d278ccc1aa63c0b4",
+    "url": "/e-commere/static/js/2.c180c8f2.chunk.js"
+  },
+  {
+    "revision": "fd13b75043dc3712e2809bca390c0314",
+    "url": "/e-commere/static/js/2.c180c8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6301f193b5cc17df5dbd",
+    "url": "/e-commere/static/js/main.da4d12b7.chunk.js"
+  },
+  {
+    "revision": "5e85b6d7a14fa8ef7055",
+    "url": "/e-commere/static/js/runtime-main.0350c4a7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/e-commere/static/media/logo.5d5d9eef.svg"
+  }
+]);
